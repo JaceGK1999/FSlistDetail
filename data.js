a2efb3d6f290459234e2ea074498bc1aaf394f71
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-08 15:01:20 -0800

    created data template and render utils

---
+++ data.js
@@ -0,0 +1,7 @@
+export const food = [
+    {
+        id: '',
+        name: '',
+        type: '',
+    },
+];
---
+++ render-utils.js
@@ -0,0 +1,21 @@
+export function renderListItem(food) {
+    const div = document.createElement('div');
+    div.classList.add('food');
+
+    const a = document.createElement('a');
+    a.href = `./animals/?id=${food.id}`;
+
+    const img = document.createElement('img');
+    img.src = `./assets/${food.type}.svg`;
+
+    const nameSpan = document.createElement('span');
+    nameSpan.textContent = food.name;
+    nameSpan.classList.add('name');
+
+    const span = document.createElement('span');
+    span.textContent = food.type;
+
+    a.append(img, nameSpan, span);
+    div.append(a);
+    return div;
+}
