0d678dc320f8d80385fa17e1287576b272eb4854
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-08 16:11:28 -0800

    added data

--- data.js
+++ data.js
@@ -1,8 +1,26 @@
 export const food = [
     {
-        id: '',
-        name: '',
-        type: '',
-        color: '',
+        id: '1',
+        name: 'carrot',
+        type: 'vegetable',
+        color: 'orange',
+    },
+    {
+        id: '2',
+        name: 'apple',
+        type: 'fruit',
+        color: 'red',
+    },
+    {
+        id: '3',
+        name: 'cashew',
+        type: 'nut',
+        color: 'tan',
+    },
+    {
+        id: '4',
+        name: 'almond',
+        type: 'nut',
+        color: 'brown',
     },
 ];
